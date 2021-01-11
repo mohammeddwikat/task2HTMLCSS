9f2d6a66e8393b8939a158db2a8c7af2e383bad7
Mohammad Dwikat <mohammed.dwikate1999@gmail.com>
2021-01-11 16:49:52 +0200

    first version

---
+++ task.js
@@ -0,0 +1,12 @@
+/*function dota(){
+    alert("gggg");
+}
+
+function showMembers(){
+    document.getElementById('AddMemebrsList').style.display='block';
+}
+
+function closeMembers(){
+    document.getElementById('AddMemebrsList').style.display='none';
+}
+*/
